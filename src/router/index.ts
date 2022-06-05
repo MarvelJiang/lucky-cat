246992ae4b4ddbe8e36c8f4d897181162ddbc1b1
jiangrenjie <jiangrenjie@hhu.edu.cn>
2022-06-05 20:32:05 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 import Money from "@/views/Money.vue";
 import labels from "@/views/labels.vue";
 import Statistics from "@/views/statistics.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
     {
         path: '/statistics',
         component: Statistics
+    },
+    {
+        path: '*',
+        component: NotFound
     },
 ];
 
