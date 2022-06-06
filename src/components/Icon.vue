d88a9565373a267f1089244be7f3ad3f5de6c6cd
jiangrenjie <jiangrenjie@hhu.edu.cn>
2022-06-06 14:25:01 +0800

    添加nav样式

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
-    <svg class="icon">
-      <use :xlink:href="'#' + name"/>
-    </svg>
-  </div>
+  <svg class="icon">
+    <use :xlink:href="'#' + name"/>
+  </svg>
 </template>
 
 <script lang="ts">
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
 
 export default {
   props: ['name'],
@@ -18,9 +20,7 @@ export default {
 
 <style lang="scss" scoped>
 .icon {
-  width: 1em; height: 1em;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
+  width: 32px;
+  height: 32px;
 }
 </style>
