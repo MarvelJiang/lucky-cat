31d753c6927c1d1096c99b17b2efe12157263ecb
jiangrenjie <jiangrenjie@hhu.edu.cn>
2022-06-07 14:10:04 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -35,6 +35,11 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    opacity: 0.7;
+  }
+
+  > .selected {
+    opacity: 1;
   }
 }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,53 @@
 <template>
   <div>
     <Layout>
-      money
+      <div>
+        <ul class="choices">
+          <li>衣服</li>
+          <li>餐饮</li>
+          <li>房租</li>
+          <li>交通</li>
+        </ul>
+        <div class="new">
+          <button>新增标签</button>
+        </div>
+      </div>
+
+      <div>
+        <label class="notes">
+          <span class="name">备注</span>
+          <input type="text">
+        </label>
+      </div>
+
+      <div>
+        <ul class="types">
+          <li class="selected">支出</li>
+          <li>收入</li>
+        </ul>
+      </div>
+
+      <div class="numberPad">
+        <div class="output">￥0</div>
+        <div class="buttons">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>今天</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>+</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>-</button>
+          <button>.</button>
+          <button>0</button>
+          <button>←</button>
+          <button>完成</button>
+        </div>
+      </div>
     </Layout>
   </div>
 </template>
